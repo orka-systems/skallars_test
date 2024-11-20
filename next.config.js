10eb3be45e573bc34a13ad33588d18a5743086f5
Majo Cuprik <marian.cuprik@icloud.com>
2024-11-20 15:35:57 +0000

    fix: update netlify and next.js config for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  }
 }
 
 module.exports = nextConfig
